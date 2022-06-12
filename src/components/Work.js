48f891a302b504aaecfb20668e3efb7ad612eb32
haydar <haydar@envepo.com>
2022-06-12 22:08:27 +0300

    proje olusturuldu ve ilk yuklemeye hazir hale getirildi

---
+++ src/components/Work.js
@@ -0,0 +1,9 @@
+const Work = () => {
+  return(
+      <ul>
+          <li>hadyar</li>
+            <li>hadyar</li>
+      </ul>
+  )
+}
+export default Work;
---
+++ src/components/WorkList.js
@@ -0,0 +1,8 @@
+import Work from './Work';
+
+const WorkList = () => {
+  return (
+      <Work/>
+  )
+}
+export default WorkList;
